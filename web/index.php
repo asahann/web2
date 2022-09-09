1608d20408a7af3361732e78a7ce62b3ef080375
Alexander Sahan <mortar5900@gmail.com>
2022-09-09 15:54:54 +0300

    Add lab1

--- web/index.php
+++ web/index.php
@@ -1,25 +1,21 @@
-<?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Веб | Лабораторна 1</title>
+		<meta charset="utf-8" />
+		<!-- CSS only -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="container">
+		<h2>Визначення кольору року</h2>
+		<form action="colorofyear.php" method="POST">
+		  <div class="mb-3">
+			<label for="exampleInputEmail1" class="form-label">Введіть рік</label>
+			<input type="text" name="year" class="form-control">
+		  </div>
+		  <button type="submit" class="btn btn-primary">Визначити</button>
+		</form>
+		</div>
+	</body>
+</html>
